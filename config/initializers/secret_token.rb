ea365151520848cb76f504721186407c8ebacc0b
Jean Lanziloti <jean.lanziloti@gmail.com>
2012-10-28 20:10:31 -0200

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seminario::Application.config.secret_token = '0adeab12e856611890845482e5e060be327941ad171ac0b4a6218a8c10848615184ce1cda45a550d399f4bd72cca245febe36688ae5659c205bbd824d681d676'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Seminario::Application.config.session_store :cookie_store, key: '_seminario_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Seminario::Application.config.session_store :active_record_store
